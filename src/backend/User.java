9e8ed6f7538348f4de9f8544d2079760c28f1ff0
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-12-01 21:39:58 +0200

    Added User class and User status enum

---
+++ src/backend/User.java
@@ -0,0 +1,16 @@
+package backend;
+
+public class User {
+  private String userId;
+  private String email;
+  private String userName;
+  private String password;
+  private String dateOfBirth;
+  private UserStatus status;
+  // will probably use hashmaps for friend lookup
+  // will probably use hashmaps for blocked lookup
+  //  private String profilePicture;
+  //  private String coverPicture;  might be a good idea to move these into a profile management class
+  //  private String bio;
+}
+
