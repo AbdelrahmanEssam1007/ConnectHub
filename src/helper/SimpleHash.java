802f8eaf6f1d5e63a0847ea590d1f31c1666fa12
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-12-01 22:00:08 +0200

    created Simple hashing algorithm using SHA-256

---
+++ src/helper/SimpleHash.java
@@ -0,0 +1,29 @@
+package helper;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SimpleHash {
+  public static String hash(String password) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("SHA-256");
+      byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+      StringBuilder hexString = new StringBuilder(2 * hash.length);
+      for (byte b : hash) {
+        String hex = Integer.toHexString(0xff & b);
+        if (hex.length() == 1) {
+          hexString.append('0');
+        }
+        hexString.append(hex);
+      }
+      return hexString.toString();
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("Algorithm not found", e);
+    }
+  }
+
+  public static boolean checkPassword(String password, String hashedPassword) {
+    return hash(password).equals(hashedPassword);
+  }
+}
