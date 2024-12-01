91e9ec696fc08d30b423e6f3b7e21bef9214b152
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-12-02 00:28:45 +0200

    Fixed Reader And Writer to be MultiObject with better formatting

--- src/backend/JSONFileReader.java
+++ src/backend/JSONFileReader.java
@@ -1,15 +1,37 @@
 package backend;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class JSONFileReader {
-    public <T> T readJson(String filePath, Class<T> CLASS) throws IOException {
+    public <T> List<T> readJson(String filePath, Class<T> CLASS) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(new File(filePath), CLASS);
+        File file = new File(filePath);
+        if (!file.exists() || file.length() == 0) {
+            return List.of();
+        }
+        return objectMapper.readValue(file, objectMapper.getTypeFactory().constructCollectionType(List.class, CLASS));
+    }
+
+    public boolean doesEmailExist(String email, String filePath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File(filePath);
+        if (!file.exists() || file.length() == 0) {
+            return false;
+        }
+        List<User> users = objectMapper.readValue(file, new TypeReference<List<User>>() {
+        });
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/backend/JSONFileWriter.java
+++ src/backend/JSONFileWriter.java
@@ -1,15 +1,21 @@
 package backend;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JSONFileWriter {
-    public void writeJson(String filePath, Object data) throws IOException {
+    public <T>void writeJson(String filePath, List<T> dataList) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(new File(filePath), data);
+        ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+        File file = new File(filePath);
+        objectWriter.writeValue(file, dataList);
     }
 }
