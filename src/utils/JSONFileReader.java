1b58b2e9581bd84a4f3f0d297dfaf4deeebfbaf6
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-12-04 14:00:08 +0200

    Fixed Dates reading and writing

--- src/utils/JSONFileReader.java
+++ src/utils/JSONFileReader.java
@@ -3,6 +3,7 @@ package utils;
 import backend.User;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +12,7 @@ import java.util.List;
 public class JSONFileReader {
     public static <T> List<T> readJson(String filePath, Class<T> CLASS) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         File file = new File(filePath);
         if (!file.exists() || file.length() == 0) {
             return List.of();
