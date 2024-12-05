938327f480ed692b688822c64b56d5fa3c57201f
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-12-05 09:48:06 +0200

    fixed implementation to friend management

--- src/backend/FriendManager.java
+++ src/backend/FriendManager.java
@@ -4,23 +4,23 @@ import java.util.List;
 
 public class FriendManager {
   private User user;
+  private Profile userProfile;
 //  private UserDB userDB;
 
   public FriendManager(User user) {
     this.user = user;
+    this.userProfile = user.getProfile();
 //    this.userDB = new UserDB();
   }
 
   // Send a friend request to another user
   public void sendFriendRequest(User targetUser) {
-    Profile userProfile = user.getProfile();
     Profile targetProfile = targetUser.getProfile();
 
     if (userProfile.getBlocked().contains(targetUser.getUserId())) {
       System.out.println("You cannot send a friend request to a user you have blocked.");
       return;
     }
-
 
     if (targetProfile.getBlocked().contains(user.getUserId())) {
       System.out.println("You are blocked by this user.");
@@ -32,67 +32,66 @@ public class FriendManager {
       return;
     }
 
+    userProfile.getPending().add(targetUser.getUserId());
     targetProfile.getPending().add(user.getUserId());
     System.out.println("Friend request sent successfully.");
   }
 
   // Accept a friend request
   public void acceptFriendRequest(User senderUser) { // will be called by passing UserDB.getInstance ().searchUserByUserName (jlist.getindex) or smth like that
-    List<String> pending = user.getProfile().getPending();
-    String senderId = senderUser.getUserId();
-    if (!pending.contains(senderId)) {
+    Profile senderProfile = senderUser.getProfile();
+
+    if (!userProfile.getPending().contains(senderUser.getUserId())) {
       System.out.println("No friend request from this user.");
       return;
     }
 
-    pending.remove(senderId);
-    user.getProfile().getFriends().add(senderId);
-    
-    senderUser.getProfile().getFriends().add(this.user.getUserId());
-    
-    System.out.println("Friend request accepted.");
+    userProfile.getPending().remove(senderUser.getUserId());
+    userProfile.getFriends().add(senderUser.getUserId());
+    senderProfile.getPending().remove(user.getUserId());
+    senderProfile.getFriends().add(user.getUserId());
   }
 
   // Decline a friend request
   public void declineFriendRequest(User senderUser) {
-    List<String> pending = user.getProfile().getPending();
-    String senderId = senderUser.getUserId();
-    if (!pending.contains(senderId)) {
+    Profile senderProfile = senderUser.getProfile();
+
+    if (!userProfile.getPending().contains(senderUser.getUserId())) {
       System.out.println("No friend request from this user.");
       return;
     }
 
-    pending.remove(senderId);
-    System.out.println("Friend request declined.");
+    userProfile.getPending().remove(senderUser.getUserId());
+    senderProfile.getPending().remove(user.getUserId());
   }
 
   // Block a user
   public void blockUser(User targetUser) {
-    Profile profile = user.getProfile();
+    Profile targetProfile = targetUser.getProfile();
     String targetId = targetUser.getUserId();
-    if (profile.getBlocked().contains(targetId)) {
+    if (userProfile.getBlocked().contains(targetId)) {
       System.out.println("User is already blocked.");
       return;
     }
 
-    profile.getBlocked().add(targetId);
-    profile.getFriends().remove(targetId);
-    profile.getPending().remove(targetId);
+    userProfile.getBlocked().add(targetId);
+    userProfile.getFriends().remove(targetId);
+    userProfile.getPending().remove(targetId);
+    targetProfile.getFriends().remove(user.getUserId());
+    targetProfile.getPending().remove(user.getUserId());
     
     System.out.println("User has been blocked.");
   }
 
   // Unblock a user
   public void unblockUser(User targetUser) {
-    List<String> blocked = user.getProfile().getBlocked();
     String targetId = targetUser.getUserId();
-    if (!blocked.contains(targetId)) {
+    if (!userProfile.getBlocked().contains(targetId)) {
       System.out.println("User is not blocked.");
       return;
     }
 
-    blocked.remove(targetId);
-    
+    userProfile.getBlocked().remove(targetId);
     System.out.println("User has been unblocked.");
   }
 }
