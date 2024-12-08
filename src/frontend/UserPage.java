741d4ef92ee22abd260055db9993e428a5f7dc4c
Ahmad Zaki <107484564+Ahmad-Zaki05@users.noreply.github.com>
2024-12-08 18:14:33 +0200

    Added profile, posts and stories functionality
    This code will need more testing to make sure it works the same as before

--- src/frontend/UserPage.java
+++ src/frontend/UserPage.java
@@ -4,7 +4,17 @@
  */
 package frontend;
 
+import backend.FriendManager;
+import backend.RefreshManager;
 import backend.User;
+import backend.UserDB;
+import backend.content.PostManager;
+import backend.content.StoryManager;
+import frontend.content.PostsPanel;
+import frontend.content.StoriesPanel;
+import java.awt.BorderLayout;
+import java.util.List;
+import javax.swing.BoxLayout;
 
 /**
  *
@@ -12,19 +22,57 @@ import backend.User;
  */
 public class UserPage extends javax.swing.JFrame {
 
-    /**
-     * Creates new form UserPage
-     */
+    private User loggedInUser;
+    private FriendManager FM;
+    private PostManager postManager;
+    private StoryManager storyManager;
+    private RefreshManager refreshManager;
+    private PostsPanel postsPanel;
+    private StoriesPanel storiesPanel;
+    private ProfilePanel profilePanel;
+    private String type = "Profile";
+    private String typeFeed = "Post";
+    
+    
     public UserPage() {
         initComponents();
         
         this.setLocationRelativeTo(null);
-        this.setTitle("ConnectHub " + "<username>");
+        this.setTitle("ConnectHub - " + "<username>");
+        
+        this.profileContentPanel.setLayout(new BorderLayout());
+        this.storiesContentPanel.setLayout(new BorderLayout());
+        this.postsContentPanel.setLayout(new BorderLayout());
+        
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                UserDB.getInstance().refreshDB();
+                UserDB.getInstance().searchUserByUserId(loggedInUser.getUserId()).setStatus(false);
+                UserDB.getInstance().SaveDB();
+            }
+        });
         
     }
     
     public UserPage (User user) {
         this ();
+        
+        this.setTitle("ConnectHub" + " - " + this.loggedInUser.getUserName());
+        
+        this.loggedInUser = user;
+        this.FM = FriendManager.getInstance(user);
+        this.postManager = new PostManager(this.loggedInUser);
+        this.storyManager = new StoryManager(this.loggedInUser);
+        this.type = "Friends";
+        this.typeFeed = "Post";
+        this.postsPanel = new PostsPanel(this.loggedInUser, postManager,this.postsContentPanel.getWidth(), this.postsContentPanel.getHeight(), type);
+        this.typeFeed = "Stories";
+        this.storiesPanel = new StoriesPanel(this.loggedInUser, storyManager, this.storiesContentPanel.getWidth(), this.storiesContentPanel.getHeight(), type);
+        this.profilePanel = new ProfilePanel(this.loggedInUser, this.profileContentPanel.getWidth(), 200);
+        this.refreshManager = new RefreshManager(List.of(postsPanel, storiesPanel));
+        
+        this.refreshButtonMouseClicked(null);
     }
 
     /**
@@ -38,7 +86,7 @@ public class UserPage extends javax.swing.JFrame {
 
         logoutButton = new javax.swing.JButton();
         refreshButton = new javax.swing.JButton();
-        jTabbedPane1 = new javax.swing.JTabbedPane();
+        mainPanel = new javax.swing.JTabbedPane();
         userProfilePanel = new javax.swing.JPanel();
         profileContentPanel = new javax.swing.JPanel();
         friendsPanel = new javax.swing.JPanel();
@@ -55,6 +103,19 @@ public class UserPage extends javax.swing.JFrame {
         logoutButton.setText("Log Out");
 
         refreshButton.setText("Refresh");
+        refreshButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                refreshButtonMouseClicked(evt);
+            }
+        });
+
+        mainPanel.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                mainPanelStateChanged(evt);
+            }
+        });
+
+        profileContentPanel.setBackground(new java.awt.Color(255, 255, 255));
 
         javax.swing.GroupLayout profileContentPanelLayout = new javax.swing.GroupLayout(profileContentPanel);
         profileContentPanel.setLayout(profileContentPanelLayout);
@@ -84,7 +145,7 @@ public class UserPage extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        jTabbedPane1.addTab("Profile", userProfilePanel);
+        mainPanel.addTab("Profile", userProfilePanel);
 
         javax.swing.GroupLayout friendsPanelLayout = new javax.swing.GroupLayout(friendsPanel);
         friendsPanel.setLayout(friendsPanelLayout);
@@ -97,7 +158,9 @@ public class UserPage extends javax.swing.JFrame {
             .addGap(0, 548, Short.MAX_VALUE)
         );
 
-        jTabbedPane1.addTab("Friends", friendsPanel);
+        mainPanel.addTab("Friends", friendsPanel);
+
+        storiesContentPanel.setBackground(new java.awt.Color(255, 255, 255));
 
         javax.swing.GroupLayout storiesContentPanelLayout = new javax.swing.GroupLayout(storiesContentPanel);
         storiesContentPanel.setLayout(storiesContentPanelLayout);
@@ -127,7 +190,9 @@ public class UserPage extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        jTabbedPane1.addTab("Friends' Stories", friendsStoriesPanel);
+        mainPanel.addTab("Friends' Stories", friendsStoriesPanel);
+
+        postsContentPanel.setBackground(new java.awt.Color(255, 255, 255));
 
         javax.swing.GroupLayout postsContentPanelLayout = new javax.swing.GroupLayout(postsContentPanel);
         postsContentPanel.setLayout(postsContentPanelLayout);
@@ -157,7 +222,7 @@ public class UserPage extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        jTabbedPane1.addTab("Friends' Posts", friendsPostsPanel);
+        mainPanel.addTab("Friends' Posts", friendsPostsPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -166,7 +231,7 @@ public class UserPage extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTabbedPane1)
+                    .addComponent(mainPanel)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(logoutButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -181,13 +246,61 @@ public class UserPage extends javax.swing.JFrame {
                     .addComponent(logoutButton)
                     .addComponent(refreshButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jTabbedPane1)
+                .addComponent(mainPanel)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void mainPanelStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_mainPanelStateChanged
+        this.refreshButtonMouseClicked(null);
+    }//GEN-LAST:event_mainPanelStateChanged
+
+    private void refreshButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_refreshButtonMouseClicked
+        UserDB.getInstance().refreshDB();
+        this.loggedInUser = UserDB.getInstance().searchUserByUserId(this.loggedInUser.getUserId());
+        this.loggedInUser = FM.refresh();
+        this.refreshManager.refreshAll();
+        this.setProfileContentPanel();
+        this.setPostsContentPanel();
+        this.setStoriesContentPanel();
+    }//GEN-LAST:event_refreshButtonMouseClicked
+
+    
+    void setProfileContentPanel () {
+        this.profileContentPanel.removeAll();
+        this.profileContentPanel.setLayout(new BoxLayout(this.profileContentPanel, BoxLayout.Y_AXIS));
+        this.type = "Profile";
+        this.postsPanel = new PostsPanel(this.loggedInUser, new PostManager(loggedInUser),this.profileContentPanel.getWidth(), this.profileContentPanel.getHeight()/2, type);
+        this.storiesPanel = new StoriesPanel(this.loggedInUser, new StoryManager(loggedInUser), this.profileContentPanel.getWidth(), this.profileContentPanel.getHeight()/2, type);
+        this.profileContentPanel.add(this.profilePanel);
+        this.profileContentPanel.add(this.postsPanel);
+        this.profileContentPanel.add(this.storiesPanel);
+        this.profileContentPanel.revalidate();
+        this.profileContentPanel.repaint();
+    }
+    
+    void setPostsContentPanel () {
+        this.postsContentPanel.removeAll();
+        this.type = "Friends";
+        this.typeFeed = "Post";
+        this.postsPanel = new PostsPanel(this.loggedInUser, new PostManager(this.loggedInUser),this.postsContentPanel.getWidth(), this.postsContentPanel.getHeight(), type);
+        this.postsContentPanel.add(this.profilePanel);
+        this.postsContentPanel.revalidate();
+        this.postsContentPanel.repaint();
+    }
+    
+    void setStoriesContentPanel () {
+        this.storiesContentPanel.removeAll();
+        this.type = "Friends";
+        this.typeFeed = "Stories";
+        this.storiesPanel = new StoriesPanel(this.loggedInUser, new StoryManager(this.loggedInUser),this.storiesContentPanel.getWidth(), this.storiesContentPanel.getHeight(), type);
+        this.storiesContentPanel.add(this.profilePanel);
+        this.storiesContentPanel.revalidate();
+        this.storiesContentPanel.repaint();
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -227,8 +340,8 @@ public class UserPage extends javax.swing.JFrame {
     private javax.swing.JPanel friendsPanel;
     private javax.swing.JPanel friendsPostsPanel;
     private javax.swing.JPanel friendsStoriesPanel;
-    private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JButton logoutButton;
+    private javax.swing.JTabbedPane mainPanel;
     private javax.swing.JPanel postsContentPanel;
     private javax.swing.JPanel profileContentPanel;
     private javax.swing.JButton refreshButton;
