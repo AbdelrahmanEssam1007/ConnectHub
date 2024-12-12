42fdf0bb9d22be5941f18a9c0d9c7424b2985bdc
Ahmad Zaki <107484564+Ahmad-Zaki05@users.noreply.github.com>
2024-12-12 23:33:25 +0200

    Modified the frontend to include pages and search

--- src/frontend/UserPage.java
+++ src/frontend/UserPage.java
@@ -102,6 +102,14 @@ public class UserPage extends javax.swing.JFrame {
         storiesContentPanel = new javax.swing.JPanel();
         friendsPostsPanel = new javax.swing.JPanel();
         postsContentPanel = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        groupsPanel = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        searchUsersButton = new javax.swing.JButton();
+        searchGroupsButton = new javax.swing.JButton();
+        searchCriteriaField = new javax.swing.JTextField();
+        searchResultsPanel = new javax.swing.JPanel();
+        createNewContentButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(600, 630));
@@ -256,6 +264,103 @@ public class UserPage extends javax.swing.JFrame {
 
         mainPanel.addTab("Friends' Posts", friendsPostsPanel);
 
+        groupsPanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout groupsPanelLayout = new javax.swing.GroupLayout(groupsPanel);
+        groupsPanel.setLayout(groupsPanelLayout);
+        groupsPanelLayout.setHorizontalGroup(
+            groupsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 576, Short.MAX_VALUE)
+        );
+        groupsPanelLayout.setVerticalGroup(
+            groupsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 536, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(groupsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(groupsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        mainPanel.addTab("Groups", jPanel2);
+
+        searchUsersButton.setText("Search Users");
+        searchUsersButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                searchUsersButtonMouseClicked(evt);
+            }
+        });
+
+        searchGroupsButton.setText("Search Groups");
+        searchGroupsButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                searchGroupsButtonMouseClicked(evt);
+            }
+        });
+
+        searchResultsPanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout searchResultsPanelLayout = new javax.swing.GroupLayout(searchResultsPanel);
+        searchResultsPanel.setLayout(searchResultsPanelLayout);
+        searchResultsPanelLayout.setHorizontalGroup(
+            searchResultsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        searchResultsPanelLayout.setVerticalGroup(
+            searchResultsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 507, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(searchResultsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(searchCriteriaField, javax.swing.GroupLayout.DEFAULT_SIZE, 362, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(searchGroupsButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(searchUsersButton)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(searchUsersButton)
+                    .addComponent(searchGroupsButton)
+                    .addComponent(searchCriteriaField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(searchResultsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        mainPanel.addTab("Search", jPanel1);
+
+        createNewContentButton.setText("Create New Content");
+        createNewContentButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                createNewContentButtonMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -267,6 +372,8 @@ public class UserPage extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(logoutButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(createNewContentButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(refreshButton)))
                 .addContainerGap())
         );
@@ -276,7 +383,8 @@ public class UserPage extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(logoutButton)
-                    .addComponent(refreshButton))
+                    .addComponent(refreshButton)
+                    .addComponent(createNewContentButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(mainPanel)
                 .addContainerGap())
@@ -320,6 +428,18 @@ public class UserPage extends javax.swing.JFrame {
         this.loggedInUser = FM.refresh();
         this.dispose();
     }//GEN-LAST:event_logoutButtonMouseClicked
+
+    private void searchGroupsButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_searchGroupsButtonMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_searchGroupsButtonMouseClicked
+
+    private void searchUsersButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_searchUsersButtonMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_searchUsersButtonMouseClicked
+
+    private void createNewContentButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_createNewContentButtonMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_createNewContentButtonMouseClicked
 
     
     void setProfileContentPanel () {
@@ -434,15 +554,23 @@ public class UserPage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton createNewContentButton;
     private javax.swing.JPanel friendsContentPanel;
     private javax.swing.JPanel friendsPanel;
     private javax.swing.JPanel friendsPostsPanel;
     private javax.swing.JPanel friendsStoriesPanel;
+    private javax.swing.JPanel groupsPanel;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JButton logoutButton;
     private javax.swing.JTabbedPane mainPanel;
     private javax.swing.JPanel postsContentPanel;
     private javax.swing.JPanel profileContentPanel;
     private javax.swing.JButton refreshButton;
+    private javax.swing.JTextField searchCriteriaField;
+    private javax.swing.JButton searchGroupsButton;
+    private javax.swing.JPanel searchResultsPanel;
+    private javax.swing.JButton searchUsersButton;
     private javax.swing.JPanel storiesContentPanel;
     private javax.swing.JPanel userProfilePanel;
     // End of variables declaration//GEN-END:variables
