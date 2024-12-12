d21195aefb57ec102872d52cb8dc27d2b7c1da97
Abdelrahman Essam <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2024-12-12 13:47:10 +0200

    small bug from merging

--- src/backend/UserDB.java
+++ src/backend/UserDB.java
@@ -96,14 +96,4 @@ public class UserDB {
     SaveDB();
   }
 
-    public void setUser(User user) {
-      for (int i = 0; i < users.size(); i++) {
-          if (users.get(i).getUserId().equals(user.getUserId())) {
-              users.set(i, user);
-              break;
-          }
-      }
-      SaveDB();
-  }
-
 }
