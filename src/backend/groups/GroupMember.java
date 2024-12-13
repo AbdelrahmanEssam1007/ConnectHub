1e25c60a1bf1eda4d33b802a731023577bc7d544
AwkAmr <es-amr.attia2027@alexu.edu.eg>
2024-12-13 03:45:50 +0200

    Group member role progress.

--- src/backend/groups/GroupMember.java
+++ src/backend/groups/GroupMember.java
@@ -1,14 +1,18 @@
 package backend.groups;
 
+import backend.User;
+
 public class GroupMember {
-  private static GroupMember GROUP_MEMBER;
+  protected User user;
+  protected Group group;
   private GroupContentManager groupContentManager;
   private GroupManager groupManager;
-  public static GroupMember getInstance() {
-    if (GROUP_MEMBER == null) {
-      GROUP_MEMBER = new GroupMember();
-    }
-    return GROUP_MEMBER;
+
+  public GroupMember(User user, Group group) {
+    this.user = user;
+    this.group = group;
+    this.groupContentManager = new GroupContentManager(user);
+    this.groupManager = GroupManager.getInstance();
   }
 
   public void addPost(){
