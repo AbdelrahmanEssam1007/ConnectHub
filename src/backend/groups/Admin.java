f466632b9abc538b47bd75b182ea50ab29364d6f
AwkAmr <es-amr.attia2027@alexu.edu.eg>
2024-12-13 05:01:31 +0200

    Added editing feature, content data is no longer final and finished most of admin role.

--- src/backend/groups/Admin.java
+++ src/backend/groups/Admin.java
@@ -1,21 +1,29 @@
 package backend.groups;
 
 import backend.User;
+import backend.content.ContentManagerFactory;
+import backend.content.Post;
+
+import java.io.File;
 
 public class Admin extends GroupMember {
   private GroupContentManager groupContentManager;
   private GroupManager groupManager;
+  private GroupDB groupDB;
 
   Admin(User user, Group group){
     super(user, group);
+    this.groupContentManager = new GroupContentManager(user);
+    this.groupManager = GroupManager.getInstance();
+    this.groupDB = GroupDB.getInstance();
   }
 
-  public void editPost(String postID, String newContent){
-
-  }
-
-  public void leaveGroup(){
-
+  public void editPost(String postID, String text, String imagePath){
+    Post post = (Post)groupContentManager.searchContentByID(postID);
+    if(text != null)
+      post.editText(text);
+    if(imagePath != null)
+      post.editImagePath(imagePath);
   }
 
   public void removeMember(){
